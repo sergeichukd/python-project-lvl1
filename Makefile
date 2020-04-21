6d3db65d0c380da954b1c16aa954d5d53e4f748e
sergeichukd <sergeychukdmitry@gmail.com>
2020-04-21 14:12:46 +0300

    flake8 and TravisCI is configured

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 install:
 	poetry install
+
+lint:
+	poetry run flake8 brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,8 +2,8 @@ import brain_games.cli
 
 
 def main():
-	brain_games.cli.welcome_user()
+    brain_games.cli.welcome_user()
 
 
 if __name__ == "__main__":
-	main()
+    main()
